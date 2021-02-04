e7eb5ffdd6f4b657c08513cf3d8fe89daeb7d086
sachin <sachin@gmail.com>
2021-02-04 13:46:09 +0530

    added if condition to check the data

--- src/main/java/com/ayansys/test/Test.java
+++ src/main/java/com/ayansys/test/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
 	public static void addData(String data) {
 		System.out.println("Data:: "+data);
+		if(data.equals("This is sachin misal")) 
+			System.out.println("Welcome");
 	}
 	
 	public static void main(String[] args) {
