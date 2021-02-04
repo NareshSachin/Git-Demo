f744f33acc858764320a19a71e8a27ec68d6447f
sachin <sachin@gmail.com>
2021-02-04 13:37:42 +0530

    added Git-Demo Project to git local repository

---
+++ src/main/java/com/ayansys/test/Test.java
@@ -0,0 +1,13 @@
+package com.ayansys.test;
+
+public class Test {
+
+	public static void addData(String data) {
+		System.out.println("Data:: "+data);
+	}
+	
+	public static void main(String[] args) {
+        addData("This is sachin misal");
+	}
+
+}
